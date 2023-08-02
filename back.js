ce7f8f6e565b62234d150753a31049a4242abd28
Om Shankar Thakur <102334161+Om-Shankar-Thakur@users.noreply.github.com>
2023-08-02 23:22:37 +0530

    initial commit

---
+++ back.js
@@ -0,0 +1,44 @@
+body {
+	background-color: #f2f2f2;
+	font-family: Arial, sans-serif;
+}
+
+h1 {
+	text-align: center;
+}
+
+form {
+	margin: 0 auto;
+	max-width: 400px;
+	padding: 20px;
+	background-color: #fff;
+	border: 1px solid #ccc;
+	border-radius: 5px;
+	box-shadow: 0 0 5px #ccc;
+}
+
+label {
+	display: block;
+	margin-bottom: 5px;
+}
+
+input[type="text"], input[type="email"], input[type="number"], select {
+	padding: 5px;
+	margin-bottom: 10px;
+	width: 100%;
+	border-radius: 3px;
+	border: 1px solid #ccc;
+}
+
+input[type="submit"] {
+	background-color: #4CAF50;
+	border: none;
+	color: #fff;
+	padding: 10px;
+	border-radius: 3px;
+	cursor: pointer;
+}
+
+input[type="submit"]:hover {
+	background-color: #3e8e41;
+}
